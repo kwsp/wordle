c184cb9608d0de92604db1bb52007b612e54219d
Tiger Nie <tnie@tuta.io>
2022-04-03 13:27:58 -0500

    init

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+import TileGrid from "./TileGrid.svelte";
+
+
+</script>
+
+<header>
+  <h1>Wordle</h1>
+</header>
+
+<main>
+  <TileGrid />
+</main>
+
+<style>
+
+header {
+  display: flex;
+  flex-direction: col;
+  justify-content: center;
+  border: 1px solid black;
+}
+
+</style>
---
+++ src/TileGrid.svelte
@@ -0,0 +1,51 @@
+<script>
+  export let guesses = [
+    ["H", "E", "L", "L", "O"],
+    ["H", "E", "", "", ""],
+  ];
+</script>
+
+<div class="board">
+  {#each guesses as guess}
+    <div class="grid-row">
+      {#each guess as char}
+        <div class={char.length ? "tile tile-used" : "tile tile-empty"}>
+          {char}
+        </div>
+      {/each}
+    </div>
+  {/each}
+</div>
+
+<style>
+  .board {
+    vertical-align: middle;
+    height: 100%;
+    margin: 2rem;
+  }
+
+  .grid-row {
+    display: flex;
+    justify-content: center;
+  }
+
+  .tile {
+    width: 3rem;
+    line-height: 3rem;
+    font-size: 2rem;
+    margin: 2px;
+    align-items: center;
+    vertical-align: middle;
+    text-align: center;
+    text-transform: uppercase;
+    user-select: none;
+  }
+
+  .tile-empty {
+    border: 2px solid grey;
+  }
+
+  .tile-used {
+    border: 2px solid black;
+  }
+</style>
